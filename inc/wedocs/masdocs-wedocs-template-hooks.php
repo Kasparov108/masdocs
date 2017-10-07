19f7fed74f24938cfed62d16b6443c99df2fc21e
Ibrahim Ibn Dawood <ibrahim@transvelo.com>
2017-10-07 21:49:15 +0530

    added hook for masdocs attributes

--- inc/wedocs/masdocs-wedocs-template-hooks.php
+++ inc/wedocs/masdocs-wedocs-template-hooks.php
@@ -4,3 +4,10 @@
  *
  * @package masdocs
  */
+
+add_filter( 'wedocs_post_type', 'masdocs_add_page_attr_to_args' );
+
+function masdocs_add_page_attr_to_args( $args ) {
+	$args['supports'][] = 'page-attributes';
+	return $args;
+}
